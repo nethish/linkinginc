b3be20a64cec6f8f76dc57dd0a8bf9e33f782915
nethish <nethish259@gmail.com>
2023-07-26 00:08:16 +0530

    Initial commit

---
+++ action_one.cpp
@@ -0,0 +1,3 @@
+int action(int a, int b) {
+  return a * b;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int action(int a, int b);
+
+int main() {
+  cout << action(3, 4) << endl;
+  return 0; 
+}
